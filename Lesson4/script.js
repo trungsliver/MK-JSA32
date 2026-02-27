2302eb0247c1fed03ad65356c741cd921b1ebae2
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-02-27 19:31:31 +0700

    Lesson 4

---
+++ Lesson4/script.js
@@ -0,0 +1,7 @@
+// Bài 1: Form đăng ký
+    // Dùng DOM để lấy các thẻ từ file HTML
+const form = document.getElementById('signUpForm');
+const message = document.getElementById('message');
+
+    // Thêm sự kiện submit cho form (khi ấn nút đăng ký)
+form.addEventListener('submit', function(event) {});
