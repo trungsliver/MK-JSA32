96ece5ecf32e97ff1759a3e0e1de2a9ce7525415
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-03-13 19:59:50 +0700

    Lesson 6

--- Lesson6/script.js
+++ Lesson6/script.js
@@ -20,3 +20,34 @@ if (savedTheme === 'dark') {
     // Nếu theme là dark, thêm class 'dark' cho body
     document.body.classList.add('dark');
 }
+
+// Bài 2: Danh sách ghi chú
+    // Lấy danh sách ghi chú từ localStorage hoặc khởi tọa danh sách rỗng
+const notes = JSON.parse(localStorage.getItem('notes')) || [];
+    // Hàm hiển thị ghi chú lên giao diện
+function displayNotes() {
+    // Dùng DOM lấy container (thẻ ul)
+    const container = document.getElementById('note-list');
+    // Xóa nội dung cũ trong container
+    container.innerHTML = '';
+    // Duyệt danh sách notes và tạo thẻ <li> cho từng note
+    notes.forEach((item, index) => {
+        container.innerHTML += `<li>${item}</li>`;
+    });
+}
+displayNotes();
+    // Xử lý sự kiện ấn nút "Thêm"
+const addBtn = document.getElementById('addNote');
+addBtn.onclick = () => {
+    let content = document.getElementById('noteInput').value.trim();
+    if (content) {
+        // Thêm ghi chú mới vào danh sách notes
+        notes.push(content);
+        // Lưu danh sách notes vào localStorage
+        localStorage.setItem('notes', JSON.stringify(notes));
+        // Hiển thị lại danh sách ghi chú
+        displayNotes();
+        // Xóa nội dung trong input sau khi thêm
+        document.getElementById('noteInput').value = '';
+    }
+};
